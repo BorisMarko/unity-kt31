9b5dca67af2b93582c72114f012954579e428f21
BorisMarko <borismarko552@gmail.com>
2023-11-30 03:26:24 +0200

    Camera update
    Добавил камере возможность настраивать размер помех, пригодится. Добавил возможности для улучшения графики(обьект PPVol, PP Layer на камере).Сделал более менее нормальный тиррейн.

--- Assets/VHSEffect/VHSPostProcessEffect.cs
+++ Assets/VHSEffect/VHSPostProcessEffect.cs
@@ -9,6 +9,7 @@ public class VHSPostProcessEffect : MonoBehaviour
 {
     public Shader shader;
     public VideoClip VHSClip;
+    public float noiseIntensity = 0.1f;
 
     private float _yScanline;
     private float _xScanline;
@@ -23,6 +24,8 @@ public class VHSPostProcessEffect : MonoBehaviour
         _player.audioOutputMode = VideoAudioOutputMode.None;
         _player.clip = VHSClip;
         _player.Play();
+
+        _material = new Material(shader);
     }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
@@ -34,21 +37,23 @@ public class VHSPostProcessEffect : MonoBehaviour
 
         _material.SetTexture("_VHSTex", _player.texture);
 
-        _yScanline += Time.deltaTime * 0.01f;
-        _xScanline -= Time.deltaTime * 0.1f;
+        _yScanline += Time.deltaTime * 0.01f * noiseIntensity;
+        _xScanline -= Time.deltaTime * 0.1f * noiseIntensity;
 
         if (_yScanline >= 1)
         {
-            _yScanline = Random.value;
+            _yScanline = Random.value * noiseIntensity;
         }
         if (_xScanline <= 0 || Random.value < 0.05)
         {
-            _xScanline = Random.value;
+            _xScanline = Random.value * noiseIntensity;
         }
+
         _material.SetFloat("_yScanline", _yScanline);
         _material.SetFloat("_xScanline", _xScanline);
         Graphics.Blit(source, destination, _material);
     }
+
 
     protected void OnDisable()
     {
